c1ceed4e1b479db824faaa8aa3507fc0ad6eb32f
ANNO-Rity <elusivejonathanaris@gmail.com>
2026-04-08 12:01:52 +0700

    FIRST RELEASE

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/main.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style scoped>
+:root {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="photo-frame">
+        <p>
+            your image
+        </p>
+    </div>
+
+    <h1>
+        your name
+    </h1>
+
+    <h2>
+        your short description
+    </h2>
+
+    <button id="button1">
+        your link 1
+    </button>
+    <br>
+    <button id="button2">
+        your link 2
+    </button>
+    <br>
+    <button id="button3">
+        your link 3
+    </button>
+    <br>
+    <button id="button4">
+        your link 4
+    </button>
+
+    <div id="footer">
+        <p>
+            made with <a href="https://vuejs.org/" target="_blank">Vue.js</a>
+            <br>
+            thank you for choosing this template for your link tree!
+        </p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+@import './style.css';
+</style>
